2031fccb9061d501436fb04a0577cb705300d8be
boris@acer <xinu@yandex.ru>
2020-11-21 11:20:19 +0300

    Updated setup.py

--- setup.py
+++ setup.py
@@ -2,5 +2,11 @@ from setuptools import setup
 
 setup(name='gym_snake',
       version='0.1.0',
+      description='Gym Snake Env',
+      url='https://github.com/boangri/gym-snake',
+      author='Boris Gribovskiy',
+      packages=['gym_snake'],
+      author_email='xinu@yandex.ru',
+      license='MIT License',
       install_requires=['gym', 'numpy', 'pygame'] 
       )
