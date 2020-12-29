ea64c200d20bd7b838017418de884f5ea88937ed
Boris Gribovskiy <xinu@yandex.ru>
2020-12-29 22:21:42 +0300

    v0.1.7

--- gym_snake/__init__.py
+++ gym_snake/__init__.py
@@ -5,4 +5,4 @@ register(
     entry_point='gym_snake.envs:SnakeEnv',
 )
 
-print('gym-snake v0.1.6 5.12.2020')
+print('gym-snake v0.1.7 29.12.2020')
--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(name='gym_snake',
-      version='0.1.6',
+      version='0.1.7',
       description='Gym Snake Env',
       url='https://github.com/boangri/gym-snake',
       author='Boris Gribovskiy',
-      packages=['gym_snake'],
+      packages=['gym_snake', 'gym_snake.envs'],
       author_email='xinu@yandex.ru',
       license='MIT License',
       install_requires=['gym', 'numpy', 'pygame'],
